68f017cfc66e3927f570ddc7afcbcfd39bb10239
Stian Hanger <pdnagilum@gmail.com>
2019-03-19 16:28:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jk JavaScript library
+Extends DOM, gives tools and app-like features.
---
+++ jk.js
@@ -0,0 +1,12 @@
+/**
+ * jk JavaScript library
+ * Extends DOM, gives tools and app-like features.
+ * 
+ * @author
+ * Stian Hanger <pdnagilum@gmail.com>
+ * 
+ * @repo
+ * https://github.com/nagilum/jk
+ */
+
+"use strict";
