613b348bc6ab872795063b8bbcbf620c7f886fad
Stian Hanger <pdnagilum@gmail.com>
2019-03-19 16:44:17 +0100

    Add info about planned v1 features

--- jk.js
+++ jk.js
@@ -10,3 +10,73 @@
  */
 
 "use strict";
+
+// #region Extend Element with functions.
+
+// TODO: .addClass
+// TODO: .append
+// TODO: .attr
+// TODO: .css
+// TODO: .empty
+// TODO: .hasClass
+// TODO: .hasAttr
+// TODO: .html
+// TODO: .on
+// TODO: .parent
+// TODO: .qs
+// TODO: .qsa
+// TODO: .remove
+// TODO: .removeClass
+// TODO: .removeAttr
+// TODO: .text
+// TODO: .toggleClass
+
+// #endregion
+
+// #region Extend Array with functions.
+
+// TODO: .all
+// TODO: .any
+// TODO: .copy
+// TODO: .distinct
+// TODO: .first
+// TODO: .orderBy
+// TODO: .orderByDescending
+// TODO: .select
+// TODO: .skip
+// TODO: .take
+// TODO: .where
+
+// #endregion
+
+// #region Extend jk with functions.
+
+// TODO: .delete
+// TODO: .get
+// TODO: .head
+// TODO: .patch
+// TODO: .post
+// TODO: .put
+
+// TODO: .qs
+// TODO: .qsa
+// TODO: .ce
+
+// TODO: .live
+
+// TODO: .route
+
+// #endregion
+
+// #region jk functions.
+// #endregion
+
+// #region Initiator.
+
+/**
+ * Init all the things..
+ */
+(() => {
+})();
+
+// #endregion
