acc562e63a73118ed572e33d6183baa1a54d2b08
Stian Hanger <pdnagilum@gmail.com>
2019-03-20 13:54:36 +0100

    Add Element prototypes

--- jk.js
+++ jk.js
@@ -13,23 +13,206 @@
 
 // #region Extend Element with functions.
 
-// TODO: .addClass
-// TODO: .append
-// TODO: .attr
-// TODO: .css
-// TODO: .empty
-// TODO: .hasClass
-// TODO: .hasAttr
-// TODO: .html
-// TODO: .on
-// TODO: .parent
-// TODO: .qs
-// TODO: .qsa
-// TODO: .remove
-// TODO: .removeClass
-// TODO: .removeAttr
-// TODO: .text
-// TODO: .toggleClass
+/**
+ * Add a CSS class.
+ * @param {String} name
+ * @returns {Element}
+ */
+Element.prototype.addClass = function (name) {
+    this.classList.add(name);
+    return this;
+};
+
+/**
+ * Append a DOM element as a child to another.
+ * @param {Element} element
+ * @returns {Element}
+ */
+Element.prototype.append = function (element) {
+    this.appendChild(element);
+    return this;
+};
+
+/**
+ * Get or set an attribute.
+ * @param {String} name
+ * @param {String} value
+ * @returns {Element | String}
+ */
+Element.prototype.attr = function (name, value) {
+    if (typeof(value) !== 'undefined') {
+        this.setAttribute(name, value);
+        return this;
+    }
+    else {
+        return this.getAttribute(name);
+    }
+};
+
+/**
+ * Get or set CSS on element.
+ * @param {String | Object} obj
+ * @returns {Element | String}
+ */
+Element.prototype.css = function (obj) {
+    let type = typeof(obj);
+
+    if (type === 'string') {
+        return window.getComputedStyle(this).getPropertyValue(obj);
+    }
+
+    for (let key in obj) {
+        if (!obj.hasOwnProperty(key)) {
+            continue;
+        }
+
+        this.style[key] = obj[key];
+    }
+
+    return this;
+};
+
+/**
+ * Remove all child nodes from an element.
+ * @returns {Element}
+ */
+Element.prototype.empty = function () {
+    while (this.firstChild) {
+        this.removeChild(this.firstChild);
+    }
+
+    return this;
+};
+
+/**
+ * Check if an element has a CSS class.
+ * @param {String} name
+ * @returns {Boolean}
+ */
+Element.prototype.hasClass = function (name) {
+    return this.classList.contains(name);
+};
+
+/**
+ * Check if an element has a specific attribute.
+ * @param {String} name
+ * @returns {Boolean}
+ */
+Element.prototype.hasAttr = function (name) {
+    return this.hasAttribute(name);
+};
+
+/**
+ * Add HTML to the element.
+ * @param {String} html
+ * @returns {Element}
+ */
+Element.prototype.html = function (html) {
+    this.innerHTML = html;
+    return this;
+};
+
+/**
+ * Add event listener.
+ * @param {String} eventName
+ * @param {Function} callback
+ * @returns {Element}
+ */
+Element.prototype.on = function (eventName, callback) {
+    this.addEventListener(eventName, callback);
+    return this;
+};
+
+/**
+ * Get the parent element.
+ * @returns {Element}
+ */
+Element.prototype.parent = function () {
+    return this.parentElement;
+};
+
+/**
+ * Query DOM for element among children of this element.
+ * @param {String} selector
+ * @returns {Element}
+ */
+Element.prototype.qs = function (selector) {
+    return this.querySelector(selector);
+};
+
+/**
+ * Query DOM for elements among children of this element.
+ * @param {String} selector
+ * @returns {Array}
+ */
+Element.prototype.qsa = function (selector) {
+    return this.querySelectorAll(selector);
+};
+
+/**
+ * Remove self from parent.
+ * @returns {Element}
+ */
+Element.prototype.remove = function () {
+    this.parentElement.removeChild(this);
+    return this;
+};
+
+/**
+ * Remove CSS class.
+ * @param {String} name
+ * @returns {Element}
+ */
+Element.prototype.removeClass = function (name) {
+    this.classList.remove(name);
+    return this;
+};
+
+/**
+ * Remove attribute.
+ * @param {String} name
+ * @returns {Element}
+ */
+Element.prototype.removeAttr = function (name) {
+    this.removeAttribute(name);
+    return this;
+};
+
+/**
+ * Add text to the element.
+ * @param {String} text
+ * @returns {Element}
+ */
+Element.prototype.text = function (text) {
+    this.innerText = text;
+    return this;
+};
+
+/**
+ * Toggle the CSS class.
+ * @param {String} name
+ * @returns {Element}
+ */
+Element.prototype.toggleClass = function (name) {
+    this.classList.toggle(name);
+    return this;
+};
+
+/**
+ * Toggle attribute.
+ * @param {String} name
+ * @returns {Element}
+ */
+Element.prototype.toggleAttr = function (name) {
+    if (this.hasAttribute(name)) {
+        this.removeAttribute(name);
+    }
+    else {
+        this.setAttribute(name, '');
+    }
+
+    return this;
+};
 
 // #endregion
 
